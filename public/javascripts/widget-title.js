fb67a39fc2b679986de549dd2e874c662a6208cb
stevenmiller888 <stevenmiller888@me.com>
2014-06-25 23:58:00 -0700

    removed close buttons

--- public/javascripts/widget-title.js
+++ public/javascripts/widget-title.js
@@ -7,28 +7,6 @@ $(function() {
     });
   });
 
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-    $(".close").style.display = "none";
-    $('.close:hover').style.display = "none";
-  }
-
-  $('.close').on('click', function(e) {
-    e.preventDefault();
-    this.parentNode.style.display = 'none';
-  });
-  $('.close1').on('click', function(e) {
-    e.preventDefault();
-    this.parentNode.style.display = 'none';
-  });
-  $('.close2').on('click', function(e) {
-    e.preventDefault();
-    this.parentNode.style.display = 'none';
-  });
-  $('.close3').on('click', function(e) {
-    e.preventDefault();
-    this.parentNode.style.display = 'none';
-  });
-
   $('.title-form').on('submit', function(e) {
     e.preventDefault();
     symbol = $('.title-input').val();
