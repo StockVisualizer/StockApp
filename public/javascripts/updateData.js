546936b6a04b91094e9691a54b3434a2594a6279
stevenmiller888 <stevenmiller888@me.com>
2014-06-27 18:29:53 -0700

    added back in market times

--- public/javascripts/updateData.js
+++ public/javascripts/updateData.js
@@ -8,7 +8,7 @@ $(function() {
     current_time = new Date().toString().substr(16, 8);
     current_time_formatted = parseInt(current_time.substr(0, 2) + current_time.substr(3, 2) + current_time.substr(6, 5));
     // The market is open from 9:30 AM to 4:00 PM Eastern Standard Time
-    // if (current_time_formatted > 93000 && current_time_formatted < 160000) {
+    if (current_time_formatted > 93000 && current_time_formatted < 160000) {
     setInterval(function() {
       var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22" + symbol + "%22%29%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json";
       $.ajax({
@@ -70,9 +70,9 @@ $(function() {
         }
       });
     }, 1000);
-    // } else {
-    //   $(".market-status").html("Market is closed");
-    // }
+    } else {
+      $(".market-status").html("Market is closed");
+    }
   } else {
     $(".market-status").html("Market is closed");
   }
